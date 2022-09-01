1a166ea42bd66c7b87866d369bab912ef49f9d96
naysw <naysoewin9@gmail.com>
2022-09-01 22:51:49 +0630

    installed auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { CommonModule } from "./common/common.module";
+import { UrlModule } from "./features/url/url.module";
 
 @Module({
-  imports: [],
+  imports: [UrlModule, CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
